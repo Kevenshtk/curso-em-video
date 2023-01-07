19460af14f35e04cf2bcc5554e46db236e71c2a0
Keven Di Camargo Elpidio <102769882+Kevenshtk@users.noreply.github.com>
2023-01-07 15:19:23 -0300

    JS aula 4

--- JavaScript/Aula 4/exer01.js
+++ JavaScript/Aula 4/exer01.js
@@ -1,5 +1,6 @@
 var vel = 70
 console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
 if(vel > 50){
     console.log(`Você ultrapassou a velocidade permitida. MUNTADO !`)
 }
--- JavaScript/Aula 4/exer02.js
+++ JavaScript/Aula 4/exer02.js
@@ -1,5 +1,6 @@
 var pais = 'brasil'
 console.log(`Vivendo em ${pais}`)
+
 if(pais == 'brasil'){
     console.log('Você é Brasileiro')
 } else {
