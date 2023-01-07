98aefec7e9088c922a9068aa83d86607f11fd359
Keven Di Camargo Elpidio <102769882+Kevenshtk@users.noreply.github.com>
2023-01-07 14:41:16 -0300

    JS aula 5

---
+++ JavaScript/Aula 4/exer01.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 50){
+    console.log(`Você ultrapassou a velocidade permitida. MUNTADO !`)
+}
+console.log('Dirija sempre com cinto de segurança !')
