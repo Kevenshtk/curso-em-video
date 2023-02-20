437feb279059b462b270e900c0d00f954cc6313b
Keven Di Camargo Elpidio <102769882+Kevenshtk@users.noreply.github.com>
2023-02-20 17:15:59 -0300

    JS aula 12

--- JavaScript/Aula 7/exer2/script.js
+++ JavaScript/Aula 7/exer2/script.js
@@ -1,1 +1,18 @@
-alert('ola')
+function gerar(){
+    var num = document.querySelector('#txtnum')
+    var tab = document.querySelector('#seltab')
+    if(num.value.length == 0){
+        alert('Por favor, digie um número')
+    } else {
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = ''
+        while(c <= 10){
+            var item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
